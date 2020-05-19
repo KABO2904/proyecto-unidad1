2e917dac927a71ed4118178653bfc79eaf33c8e3
KABO2904 <65477165+KABO2904@users.noreply.github.com>
2020-05-19 19:56:33 -0400

    avance en el diseño
    solo un poco de diseño y implementacion de los movimientos del personaje

--- money & honey.c
+++ money & honey.c
@@ -5,8 +5,13 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+
+
+
+
 int main()
-{initialize("Ejemplo uFramework", 800, 600, false);
+{initialize("Honey & Money", 800, 600, false);
 create_sprite("s_player1", 10);
 create_sprite("s_player", 10);
 create_sprite("s_player2", 1);
@@ -19,19 +24,56 @@ create_sprite("s_block",1);
 create_sprite("s_block1",1);
 create_sprite("s_block2",1);
 create_sprite("s_block3",1);
+create_sprite("s_block4",1);
+create_sprite("s_block5",1);
+create_sprite("s_block6",1);
+create_sprite("s_block7",1);
+create_sprite("s_block8",1);
+create_sprite("s_block9",1);
+create_sprite("s_block10",1);
+create_sprite("s_block11",1);
+create_sprite("s_block12",1);
+create_sprite("s_telon",1);
+create_sprite("s_comp",1);
+create_sprite("s_comp2",1);
 create_sprite("s_background",1);
+create_sprite("s_obs",1);
+create_sprite("s_obs1",1);
+create_sprite("s_obs2",1);
+create_sprite("s_obs3",1);
+create_sprite("s_obs4",1);
+create_sprite("s_obs5",1);
+create_sprite("s_obs6",1);
+create_sprite("s_obs7",1);
+create_sprite("s_obs8",1);
 
 
-int x_wall;
-int y_wall;
-x_wall=0;
-y_wall=536;
-
+int x_cat2;
+int x_player;
+int y_player;
+    x_player=370;
+    y_player=25;
+    x_cat2=x_player+64;
 
 
 
 add_frame_to_sprite("s_background","block_07.png");
 
+add_frame_to_sprite("s_obs","crate_02.png");
+add_frame_to_sprite("s_obs1","crate_22.png");
+
+
+add_frame_to_sprite("s_comp2","crate_08.png");
+add_frame_to_sprite("s_comp","crate_08 - copia.png");
+add_frame_to_sprite("s_block9","block_05 - copia (7).png");
+add_frame_to_sprite("s_block10","block_05 - copia (8).png");
+add_frame_to_sprite("s_block11","block_05 - copia (9).png");
+add_frame_to_sprite("s_block12","block_05 - copia (10).png");
+add_frame_to_sprite("s_block8","block_05 - copia.png");
+add_frame_to_sprite("s_block5","block_05 - copia (3).png");
+add_frame_to_sprite("s_block6","block_05 - copia (4).png");
+add_frame_to_sprite("s_block7","block_05 - copia (5).png");
+add_frame_to_sprite("s_block4","block_05 - copia (2).png");
 add_frame_to_sprite("s_block","crate_03.png");
 add_frame_to_sprite("s_block1","crate_15.png");
 add_frame_to_sprite("s_block2","crate_06.png");
@@ -41,6 +83,7 @@ add_frame_to_sprite("s_block3","block_05.png");
 
 
 add_frame_to_sprite("s_player0", "player_23.png");
+
 
 add_frame_to_sprite("s_player", "player_23.png");
 add_frame_to_sprite("s_player", "player_24.png");
@@ -71,30 +114,43 @@ add_frame_to_sprite("s_player5","player_04.png");
 
 
 {add_indexed_object("o_background",-200,0,1200,600,"s_background");
+{
+    add_indexed_object("o_player", x_player, y_player, 64, 64, "s_player0");
 
 {
-    add_indexed_object("o_player", 370, 20, 64, 64, "s_player0");
-{
+    add_indexed_object("o_obs",590,184,64,64,"s_obs");
     add_indexed_object("o_crate2",306,144,192,64,"s_block2");
     add_indexed_object("o_box",370,184,64,64,"s_block1");
-    add_indexed_object("o_crate",50,120,64,64,"s_block");
-    add_indexed_object("o_3",x_wall,y_wall,64,64,"s_block3");
-    if (x_wall=800==false)
-{
-        move_indexed_object_origin("o_3",64,0);
-        x_wall=x_wall+64;
+    add_indexed_object("o_crate",590,144,64,64,"s_block");
+    add_indexed_object("o_obs1",590,224,64,64,"s_obs1");
+
+
+    //encima de todo
+
+    {
+
+    add_indexed_object("o_1",0,568,160,64,"s_block3");
+    add_indexed_object("o_2",160,568,160,64,"s_block4");
+    add_indexed_object("o_3",320,568,160,64,"s_block5");
+    add_indexed_object("o_4",480,568,160,64,"s_block6");
+    add_indexed_object("o_5",640,568,160,64,"s_block7");
+    add_indexed_object("o_6",0,-32,160,64,"s_block8");
+    add_indexed_object("o_7",160,-32,160,64,"s_block9");
+    add_indexed_object("o_8",320,-32,160,64,"s_block10");
+    add_indexed_object("o_9",480,-32,160,64,"s_block11");
+    {add_indexed_object("o_co2",736,0,64,600,"s_comp2");
+    add_indexed_object("o_co",0,0,64,600,"s_comp");
+    add_indexed_object("o_10",640,-32,160,64,"s_block12");
+
+
+    }
+
+
+    }
+
 }
-
-
-
-
-
-int izq;
-int der;
-int up;
-int down;
-
-
+}
+}
 while(!is_window_closed())
 {
     bool k_left = is_key_pressed("left");
@@ -104,6 +160,42 @@ while(!is_window_closed())
 
 
 
+
+    if(k_right==false&k_left==false&k_up==false&k_down==false)
+            {
+                set_indexed_object_sprite("o_player","s_player0");
+            }
+
+     if(k_left == true&k_down==false&k_up==false&k_right==false)
+    {
+        set_indexed_object_sprite("o_player","s_player3");
+        move_indexed_object_origin("o_player", -1, 0);
+        x_player==x_player-1;
+    }
+     if ( k_right == true&k_down==false&k_up==false&k_left==false )
+    {
+      if (x_player<=550)
+
+
+    {
+        set_indexed_object_sprite("o_player", "s_player1");
+        move_indexed_object_origin("o_player", +1, 0);
+        x_player==x_player+1;
+    }
+
+    }
+    if(k_down==true&k_up==false)
+    {
+        set_indexed_object_sprite("o_player","s_player");
+        move_indexed_object_origin("o_player",0,+1);
+        y_player==y_player+1;
+    }
+    if (k_up==true&k_down==false)
+    {
+        set_indexed_object_sprite("o_player","s_player5");
+        move_indexed_object_origin("o_player",0,-1);
+        y_player==y_player==-1;
+    }
 
 
 
