2243bf4560e5e59a0e90ee2aa94fea89c519d47d
KABO2904 <65477165+KABO2904@users.noreply.github.com>
2020-05-21 00:14:49 -0400

    inclusion de paredes y trackeo del personaje
    paredes

--- money & honey.c
+++ money & honey.c
@@ -12,6 +12,14 @@
 
 int main()
 {initialize("Honey & Money", 800, 600, false);
+
+
+
+
+
+
+
+
 create_sprite("s_player1", 10);
 create_sprite("s_player", 10);
 create_sprite("s_player2", 1);
@@ -48,12 +56,7 @@ create_sprite("s_obs7",1);
 create_sprite("s_obs8",1);
 
 
-int x_cat2;
-int x_player;
-int y_player;
-    x_player=370;
-    y_player=25;
-    x_cat2=x_player+64;
+
 
 
 
@@ -79,7 +82,7 @@ add_frame_to_sprite("s_block1","crate_15.png");
 add_frame_to_sprite("s_block2","crate_06.png");
 add_frame_to_sprite("s_block3","block_05.png");
 
-{
+
 
 
 add_frame_to_sprite("s_player0", "player_23.png");
@@ -110,18 +113,34 @@ add_frame_to_sprite("s_player5","player_02.png");
 add_frame_to_sprite("s_player5","player_03.png");
 add_frame_to_sprite("s_player5","player_02.png");
 add_frame_to_sprite("s_player5","player_04.png");
-}
+
+
+
+float x_player;
+float y_player;
+
+    x_player=370;
+    y_player=25;
+
+
+    float speed;
+    speed=1;
+
+float x_block;
+float y_block;
+x_block=590;
+y_block=144;
 
 
 {add_indexed_object("o_background",-200,0,1200,600,"s_background");
 {
-    add_indexed_object("o_player", x_player, y_player, 64, 64, "s_player0");
+    add_indexed_object("o_player",x_player , y_player, 64, 64, "s_player0");
 
 {
     add_indexed_object("o_obs",590,184,64,64,"s_obs");
     add_indexed_object("o_crate2",306,144,192,64,"s_block2");
     add_indexed_object("o_box",370,184,64,64,"s_block1");
-    add_indexed_object("o_crate",590,144,64,64,"s_block");
+    add_indexed_object("o_crate",x_block,y_block,64,64,"s_block");
     add_indexed_object("o_obs1",590,224,64,64,"s_obs1");
 
 
@@ -151,6 +170,11 @@ add_frame_to_sprite("s_player5","player_04.png");
 }
 }
 }
+
+
+
+
+
 while(!is_window_closed())
 {
     bool k_left = is_key_pressed("left");
@@ -162,40 +186,43 @@ while(!is_window_closed())
 
 
     if(k_right==false&k_left==false&k_up==false&k_down==false)
-            {
-                set_indexed_object_sprite("o_player","s_player0");
-            }
+    {
+     set_indexed_object_sprite("o_player","s_player0");
+    }
 
      if(k_left == true&k_down==false&k_up==false&k_right==false)
     {
-        set_indexed_object_sprite("o_player","s_player3");
-        move_indexed_object_origin("o_player", -1, 0);
-        x_player==x_player-1;
-    }
-     if ( k_right == true&k_down==false&k_up==false&k_left==false )
+     if (x_player+7>=64)
     {
-      if (x_player<=550)
-
-
-    {
-        set_indexed_object_sprite("o_player", "s_player1");
-        move_indexed_object_origin("o_player", +1, 0);
-        x_player==x_player+1;
+     set_indexed_object_sprite("o_player","s_player3");
+     x_player=x_player-speed;
     }
-
+    }
+     else if ( k_right == true&k_down==false&k_up==false&k_left==false )
+    {
+         if (x_player+57<=736)
+        {
+     set_indexed_object_sprite("o_player", "s_player1");
+     x_player=x_player+speed;
+        }
     }
     if(k_down==true&k_up==false)
     {
-        set_indexed_object_sprite("o_player","s_player");
-        move_indexed_object_origin("o_player",0,+1);
-        y_player==y_player+1;
-    }
-    if (k_up==true&k_down==false)
+    if (y_player+60<=568)
     {
-        set_indexed_object_sprite("o_player","s_player5");
-        move_indexed_object_origin("o_player",0,-1);
-        y_player==y_player==-1;
+     set_indexed_object_sprite("o_player","s_player");
+     y_player=y_player+speed;
     }
+    }
+     else if (k_up==true&k_down==false)
+    {
+    if (y_player+10>=36)
+    {
+     set_indexed_object_sprite("o_player","s_player5");
+     y_player=y_player-speed;
+    }
+    }
+     set_indexed_object_origin("o_player",x_player,y_player);
 
 
 
